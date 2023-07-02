ee990251bc547fc7f3381ce80ed989f1ce7e511e
vinisi12363 <viniciuspv.si@gmail.com>
2023-07-02 11:49:10 -0300

    sei la oq q houve

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -3,6 +3,7 @@ import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
 import enrollmentsService from '@/services/enrollments-service';
 import addressRepository from '@/repositories/address-repository';
+import { string } from 'joi';
 
 export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
@@ -32,13 +33,13 @@ export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, re
 
 export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
 
-  const cep:string = req.query.cep.toString();
+  const cep:string= req.query.cep.toString();
   console.log("CEP", cep)
   
     try {
       const address = await enrollmentsService.getAddressFromCEP(cep);
       res.status(httpStatus.OK).send(address);
-    } catch (error) {
+    } catch (error:any) {
       if (error.name === 'NotFoundError') {
         return res.send(httpStatus.NO_CONTENT);
       }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import app, { init } from '@/app';
 
-const port = +process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 init().then(() => {
   app.listen(port, () => {
-    /* eslint-disable-next-line no-console */
+      /* eslint-disable-next-line no-console */
     console.log(`Server is listening on port ${port}.`);
   });
 });
